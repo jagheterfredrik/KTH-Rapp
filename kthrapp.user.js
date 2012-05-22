2b70aef366a35bea7ee93ed9fc30e845ad5abbbf
Fredrik Gustafsson <fredrikg@spotify.com>
2012-05-22 09:58:01 +0200

    Initial commit

---
+++ kthrapp.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           RAPP sane size
+// @author         frgustaf@kth.se
+// @namespace      all
+// @description    Calculates your mean grade at Mina sidor @ KTH
+// @include        https://rapp.csc.kth.se/*
+// @version        0.1
+// @date           2012-05-22
+// ==/UserScript==
+
+document.getElementById("kursdef").style.height = '500px';
