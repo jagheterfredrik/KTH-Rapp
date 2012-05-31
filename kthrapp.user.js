c5c1f247e2c5ed20ae693d2b90d8a1c90b690989
Fredrik Gustafsson <frgustaf@kth.se>
2012-05-31 15:31:50 +0300

    Change in comment

--- kthrapp.user.js
+++ kthrapp.user.js
@@ -2,7 +2,7 @@
 // @name           RAPP sane size
 // @author         frgustaf@kth.se
 // @namespace      all
-// @description    Calculates your mean grade at Mina sidor @ KTH
+// @description    Resizes the choose course box @ KTH Rapp
 // @include        https://rapp.csc.kth.se/*
 // @version        0.1
 // @date           2012-05-22
